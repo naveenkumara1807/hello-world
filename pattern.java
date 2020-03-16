fa0996f7421c46abaa8f1ba003fcee8aa8dcd1a6
naveenkumara1807 <61401507+naveenkumara1807@users.noreply.github.com>
2020-03-16 11:37:32 +0530

    cloud pattern  program in java

---
+++ pattern.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Pattern
+{
+    public static void main(String args[])
+    {
+        int i,j;
+        for(i=1;i<=9;i++)
+        {
+            for( j=1;j<i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
